f46eb24104e2b6bd2b94f06a6dcfe0ee32c952a5
Naumenko Evgenii <evgeniynaumenko2017@gmail.com>
2024-10-07 12:58:05 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.Win32;
+
+namespace UserLastAccessApp
+{
+    class Program
+    {
+        private const string RegistryKeyPath = @"HKEY_CURRENT_USER\Software\UserLastAccessApp";
+        private const string LastAccessKey = "LastAccessTime";
+        static void Main(string[] args)
+        {
+
+            string lastAccessTime = (string)Registry.GetValue(RegistryKeyPath, LastAccessKey, null);
+
+            if (lastAccessTime == null)
+            {
+                Console.WriteLine("Нет данных о последнем входе.");
+            }
+            else
+            {
+                Console.WriteLine($"Последний вход в программу был: {lastAccessTime}");
+            }
+            string currentTime = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
+            Registry.SetValue(RegistryKeyPath, LastAccessKey, currentTime);
+
+            Console.WriteLine($"Текущее время {currentTime} было записано в реестр как время последнего входа.");
+
+            // Завершение программы
+            Console.WriteLine("Нажмите любую клавишу для выхода...");
+            Console.ReadKey();
+        }
+    }
+}
